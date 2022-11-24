0c422fd83eee39160da2756709fdbd0ad3d81bcb
rohan soni <rohansoni54476@gmail.com>
2022-11-24 10:48:57 +0530

    add utils folder

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public String wrapQuotesString(String str) {
         return "\"" + str + "\"";
     }
+
+    public String getFormatedString() {
+        return "formated string";
+    }
 }
