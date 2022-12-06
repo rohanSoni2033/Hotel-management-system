72c4952a884f63c3cfe3d4d2ac8a35b6d13f1a46
rohan soni <rohansoni54476@gmail.com>
2022-12-06 14:56:16 +0530

    add closeStatement and closeResultSet utility function

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,11 +1,33 @@
 package utils;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import models.Booking;
+
 public class Utils {
-    public String wrapQuotesString(String str) {
+    public static String wrapQuotesString(String str) {
         return "\"" + str + "\"";
     }
 
-    public String getFormatedString() {
-        return "formated string";
+    public static void closeStatement(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException exception) {
+                System.out.println("failed to close the statement " + exception);
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException exception) {
+                System.out.println("failed to close the result set " + exception);
+            }
+        }
     }
 }
